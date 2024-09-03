0665fff6d3bf53f6d11bf31512a48b6657ebd6f5
Ugo <nozolo96@gmail.com>
2024-09-03 19:17:24 +0100

    Day 2 wip

--- day2/src/main.cpp
+++ day2/src/main.cpp
@@ -1,5 +1,34 @@
+#include <array>
+#include <cstddef>
+#include <vector>
+#include <iostream>
 #include <cstdlib>
+#include <fstream>
+
+#include "../include/header.hpp"
+
+#define NUMBER_INDEX 5 
 
 int main(int charc, char** argv){
+	std::ifstream in("input.txt");
+	std::string currLine;
+
+
+	if (in.is_open()) {
+		while (std::getline(in, currLine)){
+			if(currLine.empty()){
+				break;
+			}
+			Game currGame;
+			setGameID(currLine, currGame);
+			setGameRounds(currLine, currGame);
+			//return 0;
+		}
+	}
+	else{
+		std::cerr << "Error: Failed to open file\n";
+	}
+
 	return EXIT_SUCCESS;
 }
+
